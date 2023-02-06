d3f54030555c86702fc47dd7f86e5b73dac2a61b
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-02-06 15:39:31 +0100

    Completato esercizio DC comics

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+    name: 'Card',
+    props: {
+        title: String,
+        src: String
+    }
+
+}
+
+</script>
+
+<template>
+     <div class="cards">
+            <div class="card">
+                <img :src="src" :alt="title">
+                <h3> {{ title }}</h3>
+            </div>
+        </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
